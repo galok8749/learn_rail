f88861477fca7d548ab40868a41ab707dc4d07da
alok <galok8749@gmail.com>
2022-06-15 17:16:11 +0530

    footer and header

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,11 +1,26 @@
 class RootController < ApplicationController
+
+	def index
+		render "index"
+	end
+
 	def about
-		puts "ABOUT PAGE IN SERVER"
+		# puts "ABOUT PAGE IN SERVER"
 		render "about"
 	end
 
 	def contact
-		puts "CONTACT PAGE IN SERVER"
+		# puts "CONTACT PAGE IN SERVER"
 		render "contact"
 	end
+
+	def api
+		@response = {
+			:status => 200,
+			:message => "hello world",
+		}
+
+		render json: @response
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get '/contact', to: 'root#contact'
-  get '/about', to: 'root#about'
+  root "root#index"
+  get '/contact', to: "root#contact"
+  get '/about', to: "root#about"
+  # get '/index', to: "root#index"
+  get '/response', to: "root#api"
 end
